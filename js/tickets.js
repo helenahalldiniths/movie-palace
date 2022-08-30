9bbb1fcd6e1dcdec522563284eff5c0e66c2e00e
HelenaHalldinITHS <helena.halldin@iths.se>
2022-08-30 11:33:11 +0200

    Add support for buying tickets

--- js/tickets.js
+++ js/tickets.js
@@ -27,19 +27,28 @@ function createSelectMovieTitle(movies) {
   let select = document.createElement("select");
   select.name = "film-titles";
   select.id = "film-titles";
-
   for (const val of movies) {
     var option = document.createElement("option");
     option.value = val;
     option.text = val.charAt(0).toUpperCase() + val.slice(1);
     select.appendChild(option);
   }
-
+  let defaultOption = getDefaultOption(select, "Välj en film:");
+  select.appendChild(defaultOption);
   var label = document.createElement("label");
   label.innerHTML = "Välj film: ";
   label.htmlFor = "film-titles";
-
   document.getElementById("container").appendChild(label).appendChild(select);
+}
+
+function getDefaultOption(select, text) {
+  var option = document.createElement("option");
+  option.value = "none";
+  option.text = text;
+  option.selected = true;
+  option.disabled = true;
+  option.hidden = "hidden";
+  return option;
 }
 
 function retrieveAgeLimit() {
@@ -50,6 +59,84 @@ function retrieveAgeLimit() {
   return age;
 }
 
+function createSelectShow(event) {
+  event.preventDefault();
+  console.log("hi");
+
+  let movie = getMovie();
+  console.log(movie);
+  let shows = [];
+
+  fetch("../json/movies.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.movies.length; i++) {
+        if (data.movies[i].title === movie) {
+          for (let j = 0; j < data.movies[i].shows.length; j++) {
+            shows.push(data.movies[i].shows[j].date);
+          }
+        }
+      }
+      console.log("shows in fetch", shows);
+      createSelectShowsDropDown(shows);
+    });
+}
+
+function getMovie() {
+  let movieSelect = document.querySelector("#film-titles");
+  let movie = movieSelect.options[movieSelect.selectedIndex].value;
+  return movie;
+}
+
+function createSelectShowsDropDown(shows) {
+  let select = document.createElement("select");
+  select.name = "shows";
+  select.id = "shows";
+  for (const val of shows) {
+    var option = document.createElement("option");
+    option.value = val;
+    option.text = val.charAt(0).toUpperCase() + val.slice(1);
+    select.appendChild(option);
+  }
+  let defaultOption = getDefaultOption(select, "Välj en show");
+  select.appendChild(defaultOption);
+  var label = document.createElement("label");
+  label.innerHTML = "Välj show: ";
+  label.htmlFor = "shows";
+  document
+    .getElementById("containerForShows")
+    .appendChild(label)
+    .appendChild(select);
+}
+
+function viewNumberOfTicketsInput(event) {
+  event.preventDefault();
+  document.querySelector("#amount-tickets-label").style.display = "block";
+  document.querySelector("#amount-tickets").style.display = "block";
+  document.querySelector("#get-tickets-button").style.display = "block";
+}
+
+function showOrder(event) {
+  event.preventDefault();
+  let title = document.querySelector("#film-titles").value;
+  let showtime = document.querySelector("#shows").value;
+  let amoutOfTickets = document.querySelector("#amount-tickets").value;
+  alert(
+    `Du har köpt ${amoutOfTickets} biljetter till filmen "${title}" med visningstid ${showtime}`
+  );
+  window.location.replace("../index.html");
+}
+
 document
   .querySelector("#accept-terms-button")
   .addEventListener("click", showMovies);
+
+document
+  .querySelector("#container")
+  .addEventListener("change", createSelectShow);
+
+document
+  .querySelector("#containerForShows")
+  .addEventListener("change", viewNumberOfTicketsInput);
+
+document.querySelector("#ticketForm").addEventListener("submit", showOrder);
